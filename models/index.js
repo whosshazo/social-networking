c5aa9340b8e2d799e27b6fbb0363d8045cf5f81b
whosshazo <ryandeshazo1@gmail.com>
2022-04-02 11:27:07 -0400

    more initial setup

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require("./User");
+const Username = require("./Username");
+const Email = require("./Email");
+const Thoughts = require("./Thoughts");
+const Friends = require("./Friends");
+
+module.exports = { User, Username, Email, Thoughts, Friends };
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/social-networking",
