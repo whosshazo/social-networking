5d477ee878ba4ba2a39a2a88cb643c04ee8fe1da
whosshazo <ryandeshazo1@gmail.com>
2022-04-02 10:36:37 -0400

    server page

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/social-networking",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set("debug", true);
+
+app.use(require("./routes"));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
