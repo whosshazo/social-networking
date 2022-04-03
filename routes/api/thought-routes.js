48c6f391f24ae681757b7ff2de962b4aed6605ad
whosshazo <ryandeshazo1@gmail.com>
2022-04-03 09:31:58 -0400

    more thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -3,3 +3,10 @@ const router = require('express').Router;
 
 router.route('/').get(getThoughts).post(createThought);
 
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
